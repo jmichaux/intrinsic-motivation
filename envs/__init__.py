fdb662019f5cf54cc1f10fb22b42bbb48a1198ee
Jon Michaux <jmichaux@ttic.edu>
2019-04-01 13:40:43 -0700

    adding env imports

---
+++ envs/__init__.py
@@ -0,0 +1,11 @@
+# Standard Fetch Environments
+from fetch.fetch_env import FetchEnv
+from fetch.standard.reach import FetchReachEnv
+from fetch.standard.push import FetchPushEnv
+from fetch.standard.slide import FetchSlideEnv
+from fetch.standard.pick_and_place import FetchPickAndPlaceEnv
+
+
+
+
+
