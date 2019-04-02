751831840c3e1814babb5894d61792154cf31f13
Jon Michaux <jmichaux@ttic.edu>
2019-04-01 20:33:27 -0700

    adding all other files

---
+++ multimodal_curiosity/__init__.py
@@ -0,0 +1,42 @@
+from gym.envs.registration import register
+
+# Standard Environments
+for reward_type in ['dense', 'sparse', 'very_sparse']:
+    if reward_type == 'dense':
+        suffix = 'Dense'
+    elif reward_type == 'sparse':
+        suffix = 'Sparse'
+    else:
+        suffix = 'VerySparse'
+    kwargs = {
+        'reward_type': reward_type,
+    }
+
+    # Fetch
+    register(
+        id='FetchSlide{}-v2'.format(suffix),
+        entry_point='multimodal_curiosity.envs:FetchSlideEnv',
+        kwargs=kwargs,
+        max_episode_steps=50,
+    )
+
+    register(
+        id='FetchPickAndPlace{}-v2'.format(suffix),
+        entry_point='multimodal_curiosity.envs:FetchPickAndPlaceEnv',
+        kwargs=kwargs,
+        max_episode_steps=50,
+    )
+
+    register(
+        id='FetchReach{}-v2'.format(suffix),
+        entry_point='multimodal_curiosity.envs:FetchReachEnv',
+        kwargs=kwargs,
+        max_episode_steps=50,
+    )
+
+    register(
+        id='FetchPush{}-v2'.format(suffix),
+        entry_point='multimodal_curiosity.envs:FetchPushEnv',
+        kwargs=kwargs,
+        max_episode_steps=50,
+    )
---
+++ multimodal_curiosity/envs/__init__.py
@@ -0,0 +1,5 @@
+from multimodal_curiosity.envs.fetch_env import FetchEnv
+from multimodal_curiosity.envs.fetch.reach import FetchReachEnv
+from multimodal_curiosity.envs.fetch.push import FetchPushEnv
+from multimodal_curiosity.envs.fetch.slide import FetchSlideEnv
+from multimodal_curiosity.envs.fetch.pick_and_place import FetchPickAndPlaceEnv
