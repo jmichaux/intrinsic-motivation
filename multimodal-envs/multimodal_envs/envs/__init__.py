d7f4d0a62c5777952764d87bbed70cf924b5b88e
Jon Michaux <jmichaux@ttic.edu>
2019-04-07 16:46:28 -0700

    removed env files

--- multimodal-envs/multimodal_envs/envs/__init__.py
+++
@@ -1,5 +0,0 @@
-from multimodal_envs.envs.fetch_env import FetchEnv
-from multimodal_envs.envs.fetch.reach import FetchReachEnv
-from multimodal_envs.envs.fetch.push import FetchPushEnv
-from multimodal_envs.envs.fetch.slide import FetchSlideEnv
-from multimodal_envs.envs.fetch.pick_and_place import FetchPickAndPlaceEnv
--- multimodal-envs/setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup
-
-setup(name='multimodal_envs',
-      version='0.0.1',
-      install_requires=['gym']
-)
