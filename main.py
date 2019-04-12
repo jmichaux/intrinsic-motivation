8db54c9f447700780111e0f0331cab6d3e6b7101
Jon Michaux <jmichaux@ttic.edu>
2019-04-12 16:08:11 -0700

    add code for dynamics model to the training loop

--- main.py
+++ main.py
@@ -10,15 +10,16 @@ import gym
 import torch
 import torch.optim as optim
 
-from models import ActorCritic
+from models import ActorCritic, FwdDyn
 from ppo import PPO
 import multimodal_envs
 from multimodal_envs.wrappers import make_vec_envs
 import utils
+from utils import compute_intrinsic_rewards
 import logger
 
 parser = argparse.ArgumentParser(description='PPO')
-parser.add_argument('--env-id', type=str, default='FetchReachDense-v1')
+parser.add_argument('--env-id', type=str, default='FetchPushDense-v1')
 parser.add_argument('--log-dir', type=str, default=None)
 parser.add_argument('--print-freq', type=int, default=10)
 parser.add_argument('--clean-dir', action='store_true')
@@ -26,14 +27,17 @@ parser.add_argument('--seed', type=int, default=1)
 parser.add_argument('--num-processes', type=int, default=4)
 parser.add_argument('--num-steps', type=int, default=2048)
 parser.add_argument('--num-updates', type=int, default=int(1e4))
-parser.add_argument('--num-epochs', type=int, default=10)
+parser.add_argument('--ppo-epochs', type=int, default=10)
+parser.add_argument('--dyn-epochs', type=int, default=10)
 parser.add_argument('--num-mini-batch', type=int, default=32)
 parser.add_argument('--clip-param', type=float, default=0.2)
 parser.add_argument('--value-coef', type=float, default=0.5)
 parser.add_argument('--entropy-coef', type=float, default=0.01)
 parser.add_argument('--grad-norm-max', type=float, default=0.5)
+parser.add_argument('--dyn-grad-norm-max', type=float, default=50)
 parser.add_argument('--use-clipped-value-loss', action='store_true')
 parser.add_argument('--use-tensorboard', action='store_true')
+parser.add_argument('--dyn-lr', type=float, default=1e-4)
 parser.add_argument('--pi-lr', type=float, default=1e-4)
 parser.add_argument('--v-lr', type=float, default=1e-3)
 parser.add_argument('--hidden-size', type=int, default=64)
@@ -51,7 +55,7 @@ if __name__ == '__main__':
 
     # setup logging
     if args.log_dir is None:
-        log_dir = utils.create_log_dir(args.env_id, force_clean=args.clean_dir)
+        log_dir = utils.create_log_dirs(args.env_id, force_clean=args.clean_dir)
     else:
         log_dir = args.log_dir
     logger.configure(log_dir, ['stdout', 'log'], tbX=args.use_tensorboard)
@@ -73,7 +77,7 @@ if __name__ == '__main__':
                 hidden_size=args.hidden_size,
                 num_steps=args.num_steps,
                 num_processes=args.num_processes,
-                num_epochs=args.num_epochs,
+                ppo_epochs=args.ppo_epochs,
                 num_mini_batch=args.num_mini_batch,
                 pi_lr=args.pi_lr,
                 v_lr=args.v_lr,
@@ -86,8 +90,13 @@ if __name__ == '__main__':
                 device=device,
                 debug=args.debug)
 
-    # create curiosity module
-    # mcm = MCM()
+    # create dynamics model and optimizer
+    dynamics_dim = envs.observation_space.shape[0] + envs.action_space.shape[0]
+    dynamics_model = FwdDyn(num_inputs=dynamics_dim,
+                      hidden_size=64,
+                      num_outputs=envs.observation_space.shape[0])
+    dynamics_model.to(device)
+    dynamics_optimizer = optim.Adam(dynamics_model.parameters(), lr=args.dyn_lr)
 
     # reset envs and initialize rollouts
     obs = envs.reset()
@@ -115,14 +124,38 @@ if __name__ == '__main__':
             agent.store_rollout(obs[1], action, action_log_probs,
                                 value, reward, done, infos)
 
+        # add intrinsic rewards
+        with torch.no_grad():
+            states = agent.rollouts.obs[:-1]
+            next_states = agent.rollouts.obs[1:]
+            actions = agent.rollouts.actions
+            intrinsic_rewards = compute_intrinsic_rewards(dynamics_model, states, actions, next_states)
+            agent.rollouts.rewards += intrinsic_rewards
+
+        # Train dynamics model
+        dyn_loss = 0
+
+        for epoch in range(args.dyn_epochs):
+            curiosity_generator = agent.rollouts.curiosity_generator(args.num_mini_batch)
+
+            for sample in curiosity_generator:
+                states, actions, next_states = sample
+                loss = compute_intrinsic_reward(dynamics_model, states, actions, next_states)
+
+                dynamics_optimizer.zero_grad()
+                loss.backward()
+                torch.nn.utils.clip_grad_norm(dynamics_model.parameters(), args.dyn_grad_norm_max)
+                dynamics.optimizer.step()
+
+                dyn_loss += loss.item()
+
+        dyn_loss /= (args.dyn_epochs + 1) * args.num_mini_batch
+
         # compute returns
         agent.compute_returns(args.gamma, args.use_gae, args.gae_lambda)
 
-        # train policy
-        # update model and rollout storage
+        # update policy and value_fn, reset rollout storage
         tot_loss, pi_loss, v_loss, entropy, kl, delta_p, delta_v =  agent.update()
-
-        # train curiosity module
 
         # log data
         current = time.time()
@@ -146,6 +179,7 @@ if __name__ == '__main__':
         logger.logkv('Loss/KL', kl)
         logger.logkv('Loss/DeltaPi', delta_p)
         logger.logkv('Loss/DeltaV', delta_v)
+        logger.logkv('Loss/Dynamics', dyn_loss)
 
         if args.use_tensorboard:
             logger.add_scalar('reward/mean', np.mean(episode_rewards), total_steps, delta_t)
@@ -159,6 +193,7 @@ if __name__ == '__main__':
             logger.add_scalar('loss/kl', kl, total_steps, delta_t)
             logger.add_scalar('loss/delta_p', delta_p, total_steps, delta_t)
             logger.add_scalar('loss/delta_v', delta_v, total_steps, delta_t)
+            logger.add_scalar('loss/dynamics', dyn_loss, total_steps, delta_t)
 
             if args.debug:
                 pass
