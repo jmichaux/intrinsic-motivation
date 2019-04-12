eb4eec900f79d3ac542ebcef95b60c2233e8e7b9
Jon Michaux <jmichaux@ttic.edu>
2019-04-12 13:53:15 -0700

    rewrote actor critic model

--- models.py
+++ models.py
@@ -1,60 +1,69 @@
+import numpy as np
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
+import torch.distributions as D
 
 from distributions import DiagGaussian
-from utils import init
+from utils import *
+
 
 class ActorCritic(nn.Module):
     def __init__(self, num_inputs, hidden_size, num_outputs):
         super(ActorCritic, self).__init__()
-        self.base = MLP(num_inputs, hidden_size)
-        self.dist = DiagGaussian(hidden_size, num_outputs)
+        self.policy = Policy(num_inputs, hidden_size, num_outputs)
+        self.value_fn = ValueFn(num_inputs, hidden_size)
 
     def forward(self):
-        raise NotImplentedError
+        raise NotImplementedError
 
     def select_action(self, obs):
-        value, actor_features = self.base(obs)
-        dist = self.dist(actor_features)
+        value, actor_features = self.value_fn(obs), self.policy(obs)
+        dist = self.policy.dist(actor_features)
         action = dist.sample()
-        action_log_probs = dist.log_prob(action)
+        action_log_probs = dist.log_probs(action)
+        dist_entropy = dist.entropy().mean()
         return value, action, action_log_probs
 
     def evaluate_action(self, obs, action):
-        value, actor_features = self.base(obs)
-        dist = self.dist(actor_features)
-        action_log_probs = dist.log_prob(action)
+        value, actor_features = self.value_fn(obs), self.policy(obs)
+
+        dist = self.policy.dist(actor_features)
+        action_log_probs = dist.log_probs(action)
         entropy = dist.entropy().mean()
+
         return value, action_log_probs, entropy
 
     def get_value(self, obs):
-        value, _ = self.base(obs)
-        return value
+        return self.value_fn(obs)
 
-class MLP(nn.Module):
-    def __init__(self, num_inputs, hidden_size):
-        super(MLP, self).__init__()
-        # weight initialization
-        init_ = lambda m: init(m,nn.init.orthogonal_,
-                               lambda x: nn.init.constant_(x, 0),
-                               5.0/3)
+class Policy(nn.Module):
+    def __init__(self, num_inputs, hidden_size, num_outputs):
+        super(Policy, self).__init__()
 
         # actor
-        self.actor = nn.Sequential(
-            init_(nn.Linear(num_inputs, hidden_size)), nn.Tanh(),
-            init_(nn.Linear(hidden_size, hidden_size)), nn.Tanh())
+        self.base = nn.Sequential(
+            init_relu(nn.Linear(num_inputs, hidden_size)), nn.Tanh(),
+            # init_relu(nn.Linear(hidden_size, hidden_size)), nn.ReLU(),
+            init_tanh(nn.Linear(hidden_size, hidden_size)), nn.Tanh())
 
-        # critic
-        self.critic = nn.Sequential(
-            init_(nn.Linear(num_inputs, hidden_size)), nn.Tanh(),
-            init_(nn.Linear(hidden_size, hidden_size)), nn.Tanh())
-        self.critic_head = init_(nn.Linear(hidden_size, 1))
+        self.dist = DiagGaussian(hidden_size, num_outputs)
 
     def forward(self, x):
-        #TODO: need to modify if using
-        # recurrent policy
-        actor_features = self.actor(x)
-        value = self.critic_head(self.critic(x))
-        return value, actor_features
+        return self.base(x)
+
+class ValueFn(nn.Module):
+    def __init__(self, num_inputs, hidden_size):
+        super(ValueFn, self).__init__()
+
+        # critic
+        self.base = nn.Sequential(
+            init_relu(nn.Linear(num_inputs, hidden_size)), nn.Tanh(),
+            # init_relu(nn.Linear(hidden_size, hidden_size)), nn.ReLU(),
+            init_relu(nn.Linear(hidden_size, hidden_size)), nn.Tanh())
+        self.head = init_(nn.Linear(hidden_size, 1))
+
+    def forward(self, x):
+        return self.head(self.base(x))
