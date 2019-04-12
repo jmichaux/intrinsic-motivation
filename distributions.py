74a8e653ea81e54e34ec364d1a91699f09b7a789
Jon Michaux <jmichaux@ttic.edu>
2019-04-12 13:48:36 -0700

    updating distributions

--- distributions.py
+++ distributions.py
@@ -1,41 +1,48 @@
 import torch
 import torch.nn as nn
 import torch.distributions as D
+from utils import *
 
 class Normal(D.Normal):
-    """
-    Wrapper for Gaussian distribution
-    """
     def mode(self):
         return self.mean
 
-    # Why?????
-    def log_prob(self, action):
-        return super().log_prob(action).sum(-1)
+    def log_probs(self, action):
+        return super().log_prob(action).sum(-1, keepdim=True)
 
     def entropy(self):
         return super().entropy().sum(-1)
 
-class DiagGaussian(nn.Module):
-    """
-    Diagonal Gaussian with fixed standard deviation
-    """
-    def __init__(self, num_inputs, num_outputs):
-        super().__init__()
-        self.mu = nn.Linear(num_inputs, num_outputs)
-        nn.init.orthogonal_(self.mu.weight.data, gain=1.0)
-        nn.init.constant_(self.mu.bias.data, 0)
-        self.log_std = nn.Parameter(torch.zeros(num_outputs))
+class AddBias(nn.Module):
+    def __init__(self, bias):
+        super(AddBias, self).__init__()
+        self._bias = nn.Parameter(bias.unsqueeze(1))
 
     def forward(self, x):
-        mean = self.mu(x)
-        return Normal(mean, self.log_std.exp())
+        if x.dim() == 2:
+            bias = self._bias.t().view(1, -1)
+        else:
+            bias = self._bias.t().view(1, -1, 1, 1)
 
-if __name__ == '__main__':
-    import numpy as np
-    num_inputs = 20
-    num_outputs = 10
-    policy = DiagGaussian(num_inputs=num_inputs, num_outputs=num_outputs)
-    x = np.random.uniform(-1, 1, num_inputs)
-    x = torch.from_numpy(x).to(torch.float).view(1,-1)
-    y = policy(x)
+        return x + bias
+
+class DiagGaussian(nn.Module):
+    def __init__(self, num_inputs, num_outputs):
+        super(DiagGaussian, self).__init__()
+
+        init_ = lambda m: init(m, nn.init.orthogonal_, lambda x: nn.init.
+                               constant_(x, 0))
+
+        self.fc_mean = init_(nn.Linear(num_inputs, num_outputs))
+        self.logstd = AddBias(torch.zeros(num_outputs))
+
+    def forward(self, x):
+        action_mean = self.fc_mean(x)
+
+        #  An ugly hack for my KFAC implementation.
+        zeros = torch.zeros(action_mean.size())
+        if x.is_cuda:
+            zeros = zeros.cuda()
+
+        action_logstd = self.logstd(zeros)
+        return Normal(action_mean, action_logstd.exp())
